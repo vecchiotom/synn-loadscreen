30768f2fbb4ebc19f3c057a272a2a37dbf03f90b
Syntasu <abouma18@hotmail.com>
2018-01-22 19:52:05 +0100

    Styling for the loading screen

--- __resource.lua
+++ __resource.lua
@@ -2,29 +2,12 @@ description 'A simple loading screen!'
 
 files {
     'index.html',
-    'css/index.css'
 
-    --Loading Image Files--
-    -- 'LoadingScreen/images/backgroundImg1.jpg',
-    -- 'LoadingScreen/images/backgroundImg2.jpg',
-    -- 'LoadingScreen/images/backgroundImg3.jpg',
-    -- 'LoadingScreen/images/backgroundImg4.jpg',
-    -- 'LoadingScreen/images/backgroundImg5.jpg',
-    -- 'LoadingScreen/images/backgroundImg6.jpg',
-    -- 'LoadingScreen/images/backgroundImg7.jpg',
-    -- 'LoadingScreen/images/backgroundImg8.jpg',
-    -- Loading Audio Files--
-    -- 'LoadingScreen/audio/gtavthemesong.ogg',
-    -- Loading Config File--
-    -- 'LoadingScreen/config.json',
-    -- Loading JS Files--
-    -- 'LoadingScreen/js/materialize.min.js',
-    -- 'LoadingScreen/js/material_load.js',
-    -- 'LoadingScreen/js/jquery.min.js',
-    -- Loading CSS Files--
-    -- 'LoadingScreen/css/animate.css',
-    -- 'LoadingScreen/css/materialize.min.css',
-    -- 'LoadingScreen/css/styles.css',
+    'css/index.css',
+    
+    'fonts/gravity.otf',
+
+    'img/logo.png',
 }
 
 loadscreen 'index.html'
